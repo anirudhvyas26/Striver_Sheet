464886d9b4d7f920f27ca8408f78c7fe122da12c
Anirudh <anirudhvyas26@gmail.com>
2024-10-10 15:31:11 +0530

    adding to recurssion3

--- src/recurssion/fibonacci.java
+++ src/recurssion/fibonacci.java
@@ -1,15 +1,29 @@
 package recurssion;
 
 public class fibonacci {
-static int fab(int n) {
-	if(n==0)
-		return 0;
-	if(n==1)
-		return 1;
-	return  fab(n-1) + fab(n-2);
-}
-public static void main(String[] args) {
-	int n=10;
-	System.out.println("the n is : "+ n + "the series is: " + fab(n));
-}
+
+    // Recursive function to calculate Fibonacci number
+    static int fab(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fab(n - 1) + fab(n - 2);
+    }
+
+    // Function to print Fibonacci series up to n terms
+    static void printFibonacciSeries(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(fab(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 10;  // Number of terms in the Fibonacci series
+
+        System.out.println("The value of Fibonacci at position " + n + " is: " + fab(n));
+        System.out.print("The Fibonacci series up to " + n + " terms is: ");
+        printFibonacciSeries(n);
+    }
 }
