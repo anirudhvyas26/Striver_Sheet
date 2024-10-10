f4b0d9a18438bf3dc8b13d071af2e53292005b7c
Anirudh <anirudhvyas26@gmail.com>
2024-10-10 15:22:15 +0530

    adding to recurssion2

--- src/recurssion/fibonacci.java
+++ src/recurssion/fibonacci.java
@@ -1,5 +1,15 @@
 package recurssion;
 
 public class fibonacci {
-
+static int fab(int n) {
+	if(n==0)
+		return 0;
+	if(n==1)
+		return 1;
+	return  fab(n-1) + fab(n-2);
+}
+public static void main(String[] args) {
+	int n=10;
+	System.out.println("the n is : "+ n + "the series is: " + fab(n));
+}
 }
